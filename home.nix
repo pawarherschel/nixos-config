05584c03677207aac98c03847bbadd9ae146a2c6
Herschel Pawar <pawarherschel@gmail.com>
2024-10-03 17:19:28 +0530

    started hyprland config but bracket imbalance

--- home.nix
+++ home.nix
@@ -157,8 +157,131 @@
       #   starship init nu | save -f ~/.cache/starship/init.nu
       # '';
     };
+
+    starship = {
+      enable = true;
+      # settings = {
+      #   username.show_always = true;
+      # };
+    };
+
   };
 
+  
+  # wayland.windowManager.hyprland = {
+  #   enable = true;
+  #   settings = {
+  #     monitor = [
+  #       ",highres,auto,auto"
+  #       ",preferred,auto,auto,mirror,eDP-1"
+  #     ];
+
+  #     xwayland.force_zero_scaling = true;
+
+  #     "$terminal" = "kitty";
+
+  #     env = [
+  #       "XCURSOR_SIZE,24"
+  #       "HYPRCURSOR_SIZE,24"
+  #       "GDK_SCALE,1"
+  #     ];
+
+  #     general = {
+  #       gaps_in = 5;
+  #       gaps_out = 20;
+
+  #       border-size = 2;
+
+  #       "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+  #       "col.inactive_border" = "rgba(595959aa)";
+
+  #       resize_on_border = false;
+
+  #       allow_tearing = false;
+
+  #       layout = "dwindle";
+  #     };
+
+  #     decoration = {
+  #       rounding = 10;
+
+  #       active_opacity = 1.0;
+  #       inactive_opacity = 1.0;
+
+  #       drop_shadow = true;
+  #       shadow_range = 4;
+  #       shadow_render_power = 3;
+  #       col.shadow = "rgba(1a1a1aee)";};
+
+  #       blur = {
+  #         enabled = true;
+  #         size = 3;
+  #         passes = 1;
+
+  #         vibrancy = 0.1696;
+  #       };
+
+  #       animations = {
+  #         # enable = true;
+
+  #         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+  #         animation = [
+  #           "windows, 1, 7, myBezier"
+  #           "windowsOut, 1, 7, default, popin 80%"
+  #           "border, 1, 10, default"
+  #           "borderangle, 1, 8, default"
+  #           "fade, 1, 7, default"
+  #           "workspaces, 1, 6, default"
+  #         ];
+  #       };
+
+  #       # dwindle = {
+  #       #   pseudotile = true;
+  #       #   preserve_split = true;
+  #       # };
+
+  #       master = {
+  #         new_status = "master";
+  #       };
+
+  #       misc = {
+  #         force_default_wallpaper = -1;
+  #         disable_hyprland_logo = false;
+  #       };
+
+  #       input = {
+  #         kb_layout = "us";
+
+  #         follow_mouse = 1;
+
+  #         sensitivity = 0;
+
+  #         touchpad.natural_scroll = true;
+  #       };
+
+  #       # gestures.workspace_swipe = false;
+
+  #       bind = [
+  #         "$mainMod, Q, exec, $terminal"
+  #         "$mainMod, F, exec, firefox"
+  #         "$mainMod, C, killactive,"
+  #         "$mainMod ALT, F, fullscreen"
+  #         "$mainMod, S, togglespecialworkspace, magic"
+  #         "$mainMod SHIFT, S, movetoworkspace, special:magic"
+  #         "$mainMod SHIFT, 1, movetoworkspace, 1"
+  #       ];
+
+  #       bindm = [
+  #         "$mainMod, mouse:272, movewindow"
+  #         "$mainMod, mouse:273, resizewindow"
+  #       ];
+
+        
+  #       windowrulev2 = ["vsuppressevent maximize, class:.* "];
+  #     };
+  #   };
+  # };
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
