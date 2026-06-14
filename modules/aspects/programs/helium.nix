664322bb6b0d09cb85381fd7e7d3f050f2ae00cc
Herschel Pawar <pawarherschel@gmail.com>
2026-06-14 15:53:16 +0530

    remove custom helium.desktop; package now ships its own

--- modules/aspects/programs/helium.nix
+++ modules/aspects/programs/helium.nix
@@ -13,37 +13,6 @@
     provides.ksakura.homeManager =
       { pkgs, ... }:
       {
-        xdg.desktopEntries.helium = {
-          name = "Helium";
-          exec = "${pkgs.helium}/bin/helium %U";
-          genericName = "Web Browser";
-          comment = "Access the Internet";
-          startupNotify = true;
-          terminal = false;
-          icon = "${pkgs.helium}/lib/helium-bin-${pkgs.helium.version}/product_logo_256.png";
-          type = "Application";
-          categories = [
-            "Network"
-            "WebBrowser"
-          ];
-          mimeType = [
-            "application/pdf"
-            "application/rdf+xml"
-            "application/rss+xml"
-            "application/xhtml+xml"
-            "application/xhtml_xml"
-            "application/xml"
-            "image/gif"
-            "image/jpeg"
-            "image/png"
-            "image/webp"
-            "text/html"
-            "text/xml"
-            "x-scheme-handler/http"
-            "x-scheme-handler/https"
-          ];
-        };
-
         xdg.mimeApps = {
           enable = true;
           defaultApplications = {
