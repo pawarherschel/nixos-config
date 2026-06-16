76ca01537772a7dbc187a93d60ac999b16fded81
Herschel Pawar <pawarherschel@gmail.com>
2026-06-16 14:32:15 +0530

    use memory plugin

--- modules/aspects/programs/opencode.nix
+++ modules/aspects/programs/opencode.nix
@@ -5,16 +5,26 @@ _: {
       environment.systemPackages = [ pkgs.opencode ];
     };
 
-    provides.ksakura.homeManager = _: {
+    provides.ksakura.homeManager = { pkgs, lib, ... }: {
       programs.opencode = {
         enable = true;
+        package = pkgs.runCommand "opencode-with-libstdc++"
+          {
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            libPath = lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ];
+          }
+          ''
+            mkdir -p $out/bin
+            makeWrapper ${lib.getExe pkgs.opencode} $out/bin/opencode \
+              --prefix LD_LIBRARY_PATH : $libPath
+          '';
         settings = {
           lsp = true;
-          shell = "nu";
+          shell = "${lib.getExe pkgs.nushell}";
           snapshot = false;
           compaction.prune = true;
+          plugin = [ "opencode-mem" ];
         };
-        web.enable = true;
       };
     };
   };
