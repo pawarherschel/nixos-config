0fc90fe4acd9dc413881e457e2fe59cb58275ab1
Herschel Pawar <pawarherschel@gmail.com>
2026-09-02 16:47:17 +0530

    add back gh, required for private repos Dx

--- modules/aspects/cli/default.nix
+++ modules/aspects/cli/default.nix
@@ -8,7 +8,7 @@
       den.aspects.programs.bat
       den.aspects.programs.carapace
       den.aspects.programs.bottom
-      # den.aspects.programs.gh
+      den.aspects.programs.gh
       den.aspects.programs.git
       den.aspects.programs.helix
       den.aspects.programs.jujutsu
