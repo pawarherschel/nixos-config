cafe134e52def4995376669593c72d5faa9d3827
Herschel Pawar <pawarherschel@gmail.com>
2026-09-10 09:50:53 +0530

    Revert "add fzf system-wide with bash completions and jj dynamic completions"
    This reverts commit 0c5911aa1ea1bee86fcb2f15d9df63328df1adb5.

--- modules/aspects/cli/default.nix
+++ modules/aspects/cli/default.nix
@@ -20,11 +20,10 @@
     ];
 
     nixos =
-      { pkgs, lib, ... }:
+      { pkgs, ... }:
       {
         environment.systemPackages = with pkgs; [
           difftastic
-          fzf
           # kdiff3
           # mergiraf
           ripgrep
@@ -32,22 +31,6 @@
           zellij
           libnotify
         ];
-
-        programs.bash.interactiveShellInit = ''
-          eval "$(${lib.getExe pkgs.fzf} --bash)"
-          source "$(${lib.getExe pkgs.jujutsu} util completion bash)"
-          _jj_wrap() {
-            case "''${COMP_WORDS[1]}" in
-              fdiff)
-                COMPREPLY=($(compgen -W "$(${lib.getExe pkgs.jujutsu} bookmark list --template 'name ++ "\n"' 2>/dev/null) @ @-" -- "''${COMP_WORDS[COMP_CWORD]}"))
-                ;;
-              *)
-                _jj "$@"
-                ;;
-            esac
-          }
-          complete -F _jj_wrap jj
-        '';
       };
   };
 }
