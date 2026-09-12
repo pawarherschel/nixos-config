6d7a66e39cbf75c1fc86669dad74dd72bc5be2fe
Herschel Pawar <pawarherschel@gmail.com>
2026-09-12 22:34:40 +0530

    force generated obsidian appearance

--- modules/aspects/programs/obsidian.nix
+++ modules/aspects/programs/obsidian.nix
@@ -7,10 +7,14 @@ _: {
         environment.systemPackages = [ pkgs.obsidian ];
       };
 
-    provides.ksakura.homeManager.programs.obsidian = {
-      enable = true;
-      cli.enable = true;
-      vaults.kats-vault.target = "kats-vault";
+    provides.ksakura.homeManager = {
+      home.file."kats-vault/.obsidian/appearance.json".force = true;
+
+      programs.obsidian = {
+        enable = true;
+        cli.enable = true;
+        vaults.kats-vault.target = "kats-vault";
+      };
     };
   };
 }
